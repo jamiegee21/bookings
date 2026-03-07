747349f7bba32f25ebcfba151348f9f06313cc59
Jamie Graizevsky <89082138+JamieGraizevsky@users.noreply.github.com>
2026-03-07 08:31:54 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,5 +42,5 @@ Route::post('/logout', function () {
     auth()->logout();
     request()->session()->invalidate();
     request()->session()->regenerateToken();
-    return redirect()->route('home');
+    return redirect()->route('login');
 })->middleware('auth')->name('logout');
